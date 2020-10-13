26ed3c77a15df5cd6d8027637e95e6208de48210
ducky123456 <54424682+ducky123456@users.noreply.github.com>
2020-10-13 18:44:26 -0400

    Create Main.java

---
+++ com/ducky/javaproject/Main.java
@@ -0,0 +1,13 @@
+package com.ducky.javaproject;
+
+/**
+ * Empty file do not use
+ */
+public class Main {
+    /**
+     * Empty main method. For future use.
+     */
+    public static void main(String[] args) {
+        
+    }
+}
